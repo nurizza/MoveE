b42b9c23df752bd12d2799498b08d57522ebae77
Izza <nur_rizqiani_24rpl@student.smktelkom-mlg.sch.id>
2017-05-14 21:11:55 +0700

    Selesai yeayyyy

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl428/movee/model/SourcesResponse.java
@@ -0,0 +1,32 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl428.movee.model;
+
+import java.util.List;
+
+/**
+ * Created by hyuam on 18/04/2017.
+ */
+
+public class SourcesResponse {
+    //    "status": "ok",
+    //    "sources": [{
+    //            "id": "abc-news-au",
+    //            "name": "ABC News (AU)",
+    //            "description": "Australia's most trusted source of local, national and world news. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.",
+    //            "url": "http://www.abc.net.au/news",
+    //            "category": "general",
+    //            "language": "en",
+    //            "country": "au",
+    //            "urlsToLogos": {
+    //                "small": "",
+    //                "medium": "",
+    //                "large": ""
+    //            },
+    //            "sortBysAvailable": ["top"]
+    //            },]
+
+    //    public String status;
+//    public List<Source> sources;
+    public String page;
+    public List<Source> results;
+
+}
